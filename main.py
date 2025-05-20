433a02210a900d01002ba4c03f46f1e9e19071bd
Levin Fischer <levinfischer264@gmail.com>
2025-05-20 13:57:03 +0200

    title rename

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import helper
-from flask import Flask, request, Response, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    items = helper.get_all()
-    return render_template('index.html', items=items)
-
+    todos = helper.get_all()
+    return render_template('index.html', items=todos)
 
 @app.route('/add', methods=["POST"])
 def add():
-    text = request.form.get("text")
-    helper.add(text)
+    title = request.form.get("text")
+    helper.add(title)
     return redirect(url_for("index"))
-
 
 @app.route('/update/<int:index>')
 def update(index):
